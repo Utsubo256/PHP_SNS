c05321c4638e57d5de9e4bebba70738cd63f733a
Utsubo256 <greeeentea256@gmail.com>
2022-07-09 18:12:38 +0900

    The htaccess file

--- includes/header.php
+++ includes/header.php
@@ -33,7 +33,7 @@ if(isset($_SESSION['username'])) {
     </div>
 
     <nav>
-      <a href="#">
+      <a href="<?php echo $userLoggedIn; ?>">
         <?php echo $user['first_name'];  ?>
       </a>
       <a href="index.php">
