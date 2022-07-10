cb8739fa23707ab7e1875546551258d824839787
Utsubo256 <greeeentea256@gmail.com>
2022-07-10 09:19:53 +0900

    Post status updates part2

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -9,7 +9,7 @@ class Post {
     $this->user_obj = new User($con, $user);
   }
 
-  public function submitPost($body, $usre_to) {
+  public function submitPost($body, $user_to) {
     $body = strip_tags($body); //Removes html tags
     $body = mysqli_real_escape_string($this->con, $body);
     $check_empty = preg_replace('/\s+/', '', $body); //Deletes all spaces
@@ -25,6 +25,18 @@ class Post {
       if($user_to == $added_by) {
         $user_to = "none";
       }
+
+      //Insert post
+      $query = mysqli_query($this->con, "INSERT INTO posts VALUES(NULL, '$body', '$added_by', '$user_to', '$date_added', 'no', 'no', '0')");
+      $returned_id = mysqli_insert_id($this->con);
+
+      //Insert notification
+
+
+      //Update post count for user
+      $num_posts = $this->user_obj->getNumPosts();
+      $num_posts++;
+      $update_query = mysqli_query($this->con, "UPDATE users SET num_posts='$num_posts' WHERE username='$added_by'");
     }
   }
 }
