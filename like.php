dcbaa0a89d769882bc9726082ad91e5655f380d6
Utsubo256 <greeeentea256@gmail.com>
2022-07-16 10:04:54 +0900

    Add like button handler

--- like.php
+++ like.php
@@ -31,8 +31,18 @@ $user_liked = $row['added_by'];
 
 $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$user_liked'");
 $row = mysqli_fetch_array($user_details_query);
+$total_user_likes = $row['num_likes'];
 
 //Like button
+if(isset($_POST['like_button'])) {
+  $total_likes++;
+  $query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE post_id='$post_id'");
+  $total_user_likes++;
+  $user_likes = mysqli_query($con, "UPDATE users SET num_likes='$total_user_likes' WHERE username='$user_liked'");
+  $insert_user = mysqli_query($con, "INSERT INTO likes VALUES(NULL, '$userLoggedIn', '$post_id')");
+
+  //Insert Notification
+}
 
 //Unlike button
 
